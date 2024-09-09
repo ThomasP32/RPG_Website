6e83fd3bf25eeff2a53617610740b931d84e4319
Thomas <thomas.perron-duveau@polymtl.ca>
2024-09-09 19:46:48 -0400

    feat(map): add start of display for map size buttons

--- client/src/app/components/map/map.component.ts
+++ client/src/app/components/map/map.component.ts
@@ -1,48 +1,51 @@
+import { NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-map',
+    selector: 'game-creation',
     standalone: true,
-    imports: [],
+    imports: [NgForOf],
     templateUrl: './map.component.html',
     styleUrls: ['./map.component.scss'],
 })
 export class MapComponent {
     map: number[][] = [];
     size: 'small' | 'medium' | 'large';
+    mapSize: number;
+    nbPlayers: number;
+    nbItems: number;
 
-    sizeConversion(size: 'small' | 'medium' | 'large'): number {
-        let convertedSize: number = 0;
-        switch (size) {
-            case 'small':
-                convertedSize = 10;
-                break;
-            case 'medium':
-                convertedSize = 15;
-                break;
-            case 'large':
-                convertedSize = 20;
-                break;
-            default:
-                // Handle unexpected values (optional)
-                console.error('Invalid size value:', size);
-                break;
-        }
-        return convertedSize;
-    }
-
-    ngOnInit() {
-        this.createMap();
-    }
-
-    createMap(size: 'small' | 'medium' | 'large') {
-        const mapSize = this.sizeConversion(size);
-        for (let i = 0; i < mapSize; i++) {
+    createMap() {
+        this.map = [];
+        for (let i = 0; i < this.mapSize; i++) {
             const row: number[] = [];
-            for (let j = 0; j < mapSize; j++) {
+            for (let j = 0; j < this.mapSize; j++) {
                 row.push(0);
             }
             this.map.push(row);
         }
+        console.log('Map:', this.map);
+    }
+
+    sizeConversion(size: 'small' | 'medium' | 'large'): void {
+        console.log('Button clicked, Size :', size);
+        switch (size) {
+            case 'small':
+                this.mapSize = 10;
+                this.nbItems = 2;
+                break;
+            case 'medium':
+                this.mapSize = 15;
+                this.nbItems = 4;
+                break;
+            case 'large':
+                this.mapSize = 20;
+                this.nbItems = 6;
+                break;
+            default:
+                console.error('Invalid size value:', size);
+                break;
+        }
+        this.createMap();
     }
 }
--- client/src/main.ts
+++ client/src/main.ts
@@ -7,21 +7,22 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { MapComponent } from 'src/app/components/map/map.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
     enableProdMode();
 }
 
-
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
+    { path: 'map', component: MapComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
 bootstrapApplication(AppComponent, {
     providers: [provideHttpClient(), provideRouter(routes), provideAnimations()],
-})
+});
