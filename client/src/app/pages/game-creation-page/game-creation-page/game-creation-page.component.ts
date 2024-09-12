bbaedb0772fc06dbc9fbaadbf070ab155b8ea7ba
anismehenni <anxs.meh@gmail.com>
2024-09-12 16:09:24 -0400

    Game creation page

---
+++ client/src/app/pages/game-creation-page/game-creation-page/game-creation-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-creation-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-creation-page.component.html',
+  styleUrl: './game-creation-page.component.scss'
+})
+export class GameCreationPageComponent {
+
+}
