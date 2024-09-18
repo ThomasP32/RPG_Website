936380444ec007a7c8f105770ed50f77e8889aa3
Emlyn <emlyn.murphy@polymtl.ca>
2024-09-18 11:47:08 -0400

    format character form

--- client/src/app/components/create-character/create-character.component.ts
+++ client/src/app/components/create-character/create-character.component.ts
@@ -16,12 +16,20 @@ export class CreateCharacterComponent {
     characterName: string = '';
     selectedCharacter = '';
     lifeOrSpeedBonus = '';
-    diceAssignment = '';
-    attackBonus = '';
-    defenseBonus = '';
 
-    life = 4;
-    speed = 4;
+    defaultPoints = 4;
+
+    life = this.defaultPoints;
+    speed = this.defaultPoints;
+
+    attack = this.defaultPoints;
+    defense = this.defaultPoints;
+
+    diceRollAttack = 0;
+    diceRollDefense = 0;
+    diceRolled: boolean = false;
+    rollingDiceFor: string = '';
+    rollingD4: boolean = true;
 
     characters: Character[] = [];
 
@@ -45,14 +53,38 @@ export class CreateCharacterComponent {
         }
     }
 
-    assignDice() {
-        if (this.diceAssignment === 'attack') {
-            this.attackBonus = 'D4';
-            this.defenseBonus = 'D6';
-        } else if (this.diceAssignment === 'defense') {
-            this.attackBonus = 'D6';
-            this.defenseBonus = 'D4';
+    rollDice(sides: number): number {
+        return Math.floor(Math.random() * sides) + 1;
+    }
+
+    rollFor(attribute: string) {
+        this.rollingDiceFor = attribute;
+        this.diceRolled = false;
+    }
+
+    assignD4() {
+        const rollResult = this.rollDice(4);
+        if (this.rollingDiceFor === 'attack') {
+            this.diceRollAttack = rollResult;
+            this.attack = this.defaultPoints + this.diceRollAttack;
+        } else if (this.rollingDiceFor === 'defense') {
+            this.diceRollDefense = rollResult;
+            this.defense = this.defaultPoints + this.diceRollDefense;
         }
+        this.rollingD4 = false;
+        this.diceRolled = true;
+    }
+
+    assignD6() {
+        const rollResult = this.rollDice(6);
+        if (this.rollingDiceFor === 'attack') {
+            this.diceRollDefense = rollResult;
+            this.defense = this.defaultPoints + this.diceRollDefense;
+        } else if (this.rollingDiceFor === 'defense') {
+            this.diceRollAttack = rollResult;
+            this.attack = this.defaultPoints + this.diceRollAttack;
+        }
+        this.diceRolled = true;
     }
 
     onSubmit() {
