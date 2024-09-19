0ed736332415b9851c7cf2253010da36dd26ac51
Emlyn <emlyn.murphy@polymtl.ca>
2024-09-19 16:22:43 -0400

    removing game related stuff

--- client/src/app/components/create-game/create-game.component.ts
+++ client/src/app/components/create-game/create-game.component.ts
@@ -1,44 +1,9 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { GameService } from '@app/services/game.service';
-import { Game } from 'src/app/interfaces/vec2';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-create-game',
     standalone: true,
-    template: `<div class="create-game-view">
-        <div class="game-list">
-            <div *ngFor="let game of availableGames" class="game-item" (click)="selectGame(game)">
-                <h3>{{ game.name }}</h3>
-                <p>Map Size: {{ game.mapSize }}</p>
-                <p>Mode: {{ game.mode }}</p>
-            </div>
-        </div>
-    </div>`,
     templateUrl: './create-game.component.html',
     styleUrls: ['./create-game.component.scss'],
 })
-export class CreateGameComponent {
-    availableGames: Game[] = [];
-    errorMessage: string = '';
-    game: Game;
-
-    private readonly gameService: GameService = inject(GameService);
-    private readonly router: Router = inject(Router);
-
-    loadAvailableGames() {
-        this.gameService.getVisibleGames().subscribe((games) => {
-            this.availableGames = games;
-        });
-    }
-
-    selectGame(game: { id: number; name: string; isVisible: boolean }) {
-        this.gameService.checkGameAvailability(game.id).subscribe((isAvailable) => {
-            if (isAvailable) {
-                this.router.navigate(['/create-character', game.id]);
-            } else {
-                this.errorMessage = 'The selected game is unavailable. Please choose another game.';
-            }
-        });
-    }
-}
+export class CreateGameComponent {}
