404370460f1ce3a15fd2a7e8d28503e85d466cf7
Emlyn <emlyn.murphy@polymtl.ca>
2024-09-19 14:35:46 -0400

    game => map

--- client/src/app/components/create-game/create-game.component.ts
+++ client/src/app/components/create-game/create-game.component.ts
@@ -3,7 +3,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { GameService } from '@app/services/game.service';
-import { Game } from 'src/app/interfaces/game';
+import { Map } from 'src/app/interfaces/map';
 
 @Component({
     selector: 'app-create-game',
@@ -13,49 +13,47 @@ import { Game } from 'src/app/interfaces/game';
     imports: [FormsModule, CommonModule],
 })
 export class CreateGameComponent {
-    availableGames: Game[] = [];
+    availableGames: Map[] = [];
     errorMessage: string = '';
-    game: Game;
+    map: Map;
 
     private readonly gameService: GameService = inject(GameService);
     private readonly router: Router = inject(Router);
 
-    // loadAvailableGames() {
-    //     this.gameService.getVisibleGames().subscribe((games) => {
-    //         this.availableGames = games;
-    //     });
-    // }
-
-    // display list of mockData games
-    loadMockAvailableGames() {
-        this.gameService.getMockVisibleGames().subscribe((games) => {
+    loadAvailableGames() {
+        this.gameService.getVisibleGames().subscribe((games) => {
             this.availableGames = games;
         });
     }
-    ngOnInit(): void {
-        this.loadMockAvailableGames();
-    }
 
-    // redirects organizer to character creation form if available
-    // if not available, error message
-    // selectGame(game: { id: string; name: string; mapSize: number; gameMode: string; mapPreview: string; lastModified: string }) {
-    //     this.gameService.checkGameAvailability(game.id).subscribe((isAvailable) => {
-    //         if (isAvailable) {
-    //             this.router.navigate(['/create-character', game.id]);
-    //         } else {
-    //             this.errorMessage = 'The selected game is unavailable. Please choose another game.';
-    //         }
-    //     });
-    // }
-
-    // mockData implementation
-    selectGame(game: Game) {
-        this.gameService.checkMockGameAvailability(game.name).subscribe((isAvailable) => {
+    selectGame(game: { id: string; name: string; mapSize: number; gameMode: string; mapPreview: string; lastModified: string }) {
+        this.gameService.checkGameAvailability(game.id).subscribe((isAvailable) => {
             if (isAvailable) {
-                this.router.navigate(['/waiting-room']);
+                this.router.navigate(['/create-character', game.id]);
             } else {
                 this.errorMessage = 'The selected game is unavailable. Please choose another game.';
             }
         });
     }
+
+    // display list of mockData games
+    // loadMockAvailableGames() {
+    //     this.gameService.getMockVisibleGames().subscribe((games) => {
+    //         this.availableGames = games;
+    //     });
+    // }
+    // ngOnInit(): void {
+    //     this.loadMockAvailableGames();
+    // }
+
+    // mockData implementation
+    // selectGame(game: Game) {
+    //     this.gameService.checkMockGameAvailability(game.name).subscribe((isAvailable) => {
+    //         if (isAvailable) {
+    //             this.router.navigate(['/waiting-room']);
+    //         } else {
+    //             this.errorMessage = 'The selected game is unavailable. Please choose another game.';
+    //         }
+    //     });
+    // }
 }
--- client/src/app/interfaces/game.ts
+++
@@ -1,7 +0,0 @@
-export interface Game {
-    name: string;
-    mapSize: number; // 10, 15, ou 20
-    gameMode: string; // classique ou ctf
-    //mapPreview: string; // matrice sauvegardé dans BD
-    lastModified: string;
-}
