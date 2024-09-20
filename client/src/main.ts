d0ce2903fa349bd0341f1db8440a79173a6e96f0
Emlyn <emlyn.murphy@polymtl.ca>
2024-09-20 14:04:36 -0400

    feat(character-creation): path to page added

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,13 +3,14 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter } from '@angular/router';
-import { StartGamePageComponent } from '@app/pages/start-game-page/start-game-page.component';
+import { CreateCharacterComponent } from '@app/components/create-character/create-character.component';
+import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageMapsComponent } from '@app/pages/material-page/material-page-map.component';
+import { StartGamePageComponent } from '@app/pages/start-game-page/start-game-page.component';
 import { environment } from './environments/environment';
-import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 
 if (environment.production) {
     enableProdMode();
@@ -20,8 +21,9 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageMapsComponent },
-    { path: 'create-game', component: StartGamePageComponent},
-    {path: 'admin-page', component: AdminPageComponent},
+    { path: 'create-game', component: StartGamePageComponent },
+    { path: 'admin-page', component: AdminPageComponent },
+    { path: 'character-form', component: CreateCharacterComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
