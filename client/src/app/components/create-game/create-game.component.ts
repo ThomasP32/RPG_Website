bc4086f51195c7db513ead3da8fde81744afecbb
Emlyn <emlyn.murphy@polymtl.ca>
2024-09-20 12:05:08 -0400

    deleting useless files

--- client/src/app/components/create-game/create-game.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-create-game',
-    standalone: true,
-    templateUrl: './create-game.component.html',
-    styleUrls: ['./create-game.component.scss'],
-})
-export class CreateGameComponent {}
