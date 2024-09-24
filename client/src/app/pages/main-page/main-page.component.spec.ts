33c64700ca8cc820d5585589615cfc55e6ae3a48
ldesmars <ldesmars2002@gmail.com>
2024-09-24 15:32:17 -0400

    tests(initial_view)

--- client/src/app/pages/main-page/main-page.component.spec.ts
+++ client/src/app/pages/main-page/main-page.component.spec.ts
@@ -1,75 +1,89 @@
-// import { HttpResponse } from '@angular/common/http';
-// import { provideHttpClientTesting } from '@angular/common/http/testing';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { Routes, provideRouter } from '@angular/router';
-// import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-// import { CommunicationService } from '@app/services/communication.service';
-// import { of, throwError } from 'rxjs';
-// import SpyObj = jasmine.SpyObj;
+import { HttpResponse } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Routes, provideRouter } from '@angular/router';
+import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { CommunicationService } from '@app/services/communication.service';
+import { of } from 'rxjs';
+import SpyObj = jasmine.SpyObj;
 
-// const routes: Routes = [];
+const routes: Routes = [];
 
-// describe('MainPageComponent', () => {
-//     let component: MainPageComponent;
-//     let fixture: ComponentFixture<MainPageComponent>;
-//     let communicationServiceSpy: SpyObj<CommunicationService>;
+describe('MainPageComponent', () => {
+    let component: MainPageComponent;
+    let fixture: ComponentFixture<MainPageComponent>;
+    let communicationServiceSpy: SpyObj<CommunicationService>;
 
-//     beforeEach(async () => {
-//         communicationServiceSpy = jasmine.createSpyObj('ExampleService', ['basicGet', 'basicPost']);
-//         communicationServiceSpy.basicGet.and.returnValue(of({ title: '', body: '' }));
-//         communicationServiceSpy.basicPost.and.returnValue(of(new HttpResponse<string>({ status: 201, statusText: 'Created' })));
+    beforeEach(async () => {
+        communicationServiceSpy = jasmine.createSpyObj('CommunicationService', ['basicGet', 'basicPost']);
+        communicationServiceSpy.basicGet.and.returnValue(of({ title: '', body: '' }));
+        communicationServiceSpy.basicPost.and.returnValue(of(new HttpResponse<string>({ status: 201, statusText: 'Created' })));
 
-//         await TestBed.configureTestingModule({
-//             imports: [MainPageComponent],
-//             providers: [
-//                 {
-//                     provide: CommunicationService,
-//                     useValue: communicationServiceSpy,
-//                 },
-//                 provideHttpClientTesting(),
-//                 provideRouter(routes),
-//             ],
-//         }).compileComponents();
-//     });
+        await TestBed.configureTestingModule({
+            imports: [MainPageComponent],
+            providers: [
+                {
+                    provide: CommunicationService,
+                    useValue: communicationServiceSpy,
+                },
+                provideHttpClientTesting(),
+                provideRouter(routes),
+            ],
+        }).compileComponents();
+    });
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(MainPageComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MainPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
 
-//     it("should have as title 'LOG2990'", () => {
-//         expect(component.title).toEqual('LOG2990');
-//     });
+    it("should have as title 'Brassbound Realms'", () => {
+        expect(component.title).toEqual('Brassbound Realms');
+    });
 
-//     it('should call basicGet when calling getMessagesFromServer', () => {
-//         component.getMessagesFromServer();
-//         expect(communicationServiceSpy.basicGet).toHaveBeenCalled();
-//     });
+    it('should display the game name in the header', () => {
+        const titleElement = fixture.debugElement.nativeElement.querySelector('.header-item');
+        expect(titleElement.textContent).toContain(component.title);
+    });
 
-//     it('should call basicPost when calling sendTimeToServer', () => {
-//         component.sendTimeToServer();
-//         expect(communicationServiceSpy.basicPost).toHaveBeenCalled();
-//     });
+    it('should display the game logo', () => {
+        const logoElement = fixture.debugElement.nativeElement.querySelector('.game-logo');
+        expect(logoElement.src).toContain(component.logoPath);
+    });
+    
+    it('should display the team number', () => {
+        const teamNumberElement = fixture.debugElement.nativeElement.querySelector('.team-number');
+        expect(teamNumberElement.textContent).toContain(component.teamNumber);
+    });
 
-//     it('should handle basicPost that returns a valid HTTP response', () => {
-//         component.sendTimeToServer();
-//         component.message.subscribe((res) => {
-//             expect(res).toContain('201 : Created');
-//         });
-//     });
+    it('should display the developers', () => {
+        const developersElement = fixture.debugElement.nativeElement.querySelector('.developers');
+        expect(developersElement.textContent).toContain(component.developers.join(', '));
+    });
 
-//     it('should handle basicPost that returns an invalid HTTP response', () => {
-//         communicationServiceSpy.basicPost.and.returnValue(throwError(() => new Error('test')));
-//         component.sendTimeToServer();
-//         component.message.subscribe({
-//             next: (res) => {
-//                 expect(res).toContain('Le serveur ne répond pas');
-//             },
-//         });
-//     });
-// });
+    it('should display a disabled "Rejoindre une Partie" button', () => {
+        const joinGameButton = fixture.debugElement.nativeElement.querySelector('.disabled');
+        expect(joinGameButton).toBeTruthy();
+        expect(joinGameButton.disabled).toBeTrue();
+    });
+
+    it('should navigate to the game creation view when "Créer une partie" is clicked', () => {
+        spyOn(component, 'navigateToCreateGame');
+        const createGameButton = fixture.debugElement.nativeElement.querySelectorAll('.button')[0];
+        createGameButton.click();
+        expect(component.navigateToCreateGame).toHaveBeenCalled();
+    });
+
+    it('should navigate to the admin view when "Administration" is clicked', () => {
+        spyOn(component, 'navigateToAdmin');
+        const adminButton = fixture.debugElement.nativeElement.querySelectorAll('.button')[1];
+        adminButton.click();
+        expect(component.navigateToAdmin).toHaveBeenCalled();
+    });
+
+});
