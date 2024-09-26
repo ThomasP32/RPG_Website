8af81cd2484e1a1bba498fcf3ca7189b2d68a964
Emlyn <emlyn.murphy@polymtl.ca>
2024-09-26 11:31:25 -0400

    refactor(create-game): removed unnecessary component

--- client/src/app/pages/start-game-page/start-game-page.component.spec.ts
+++ client/src/app/pages/start-game-page/start-game-page.component.spec.ts
@@ -1,16 +1,49 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from '@app/pages/app/app.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Routes, provideRouter } from '@angular/router';
+import { CommunicationMapService } from '@app/services/communication.map.service';
+import { Map } from '@common/map.types';
+import { of } from 'rxjs';
+import { StartGamePageComponent } from './start-game-page.component';
 
-describe('AppComponent', () => {
+import SpyObj = jasmine.SpyObj;
+
+const routes: Routes = [];
+
+describe('StartGamePageComponent', () => {
+    let component: StartGamePageComponent;
+    let fixture: ComponentFixture<StartGamePageComponent>;
+    let communicationMapServiceSpy: SpyObj<CommunicationMapService>;
+    let mockMaps: Map[];
+
     beforeEach(async () => {
+        communicationMapServiceSpy = jasmine.createSpyObj('CommunicationMapService', ['getMapsFromServer', 'maps$']);
+
+        communicationMapServiceSpy.maps$ = of(mockMaps);
+
+        mockMaps = [
+            { _id: '1', name: 'Map 1', isVisible: true, mapSize: { x: 10, y: 10 }, startTiles: [], items: [], doorTiles: [], tiles: [] },
+            { _id: '2', name: 'Map 2', isVisible: false, mapSize: { x: 10, y: 10 }, startTiles: [], items: [], doorTiles: [], tiles: [] },
+        ];
+
         await TestBed.configureTestingModule({
-            imports: [AppComponent],
+            imports: [StartGamePageComponent],
+            providers: [
+                {
+                    provide: CommunicationMapService,
+                    useValue: communicationMapServiceSpy,
+                },
+                provideRouter(routes),
+            ],
         }).compileComponents();
     });
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
+    beforeEach(() => {
+        const fixture = TestBed.createComponent(StartGamePageComponent);
+        const component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
     });
 });
