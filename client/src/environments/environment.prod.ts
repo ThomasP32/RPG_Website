51e6624fb1457fdaf0fb433f9d622aa197c8fb7c
Thomas <thomas.perron-duveau@polymtl.ca>
2024-10-11 18:03:49 -0400

    fix(deployment) : fix environment variables for deployments

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'myServerURL', // TODO: Add remote server URL
+    serverUrl: 'http://polytechnique-montr-al.gitlab.io/log2990/20243/equipe-109/LOG2990-109/api', // TODO: Add remote server URL
 };
