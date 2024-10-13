7f5f8f7d1246279197c70129b7892d05d1a26946
txciturne <celineouchiha@gmail.com>
2024-10-12 22:10:13 -0400

    test(mapcounterservice) : all tests pass 100% coverage

--- client/src/app/services/map-counter.service.spec.ts
+++ client/src/app/services/map-counter.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { MapCounterService } from './map-counter.service';
 
 describe('MapCounterService', () => {
@@ -8,44 +7,43 @@ describe('MapCounterService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(MapCounterService);
-        service.startingPointCounter = 0;
-        service.randomItemCounter = 0;
-        service.itemsCounter = 0;
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
 
-    // it('should update starting point counter', () => {
-    //     let counter = 0;
-    //     service.startingPointCounter$.subscribe((value) => (counter = value));
-
-    //     service.updateCounters('starting-point', 'add');
-    //     expect(counter).toBe(1);
-
-    //     service.updateCounters('starting-point', 'remove');
-    //     expect(counter).toBe(0);
-    // });
-
-    // it('should update items counter', () => {
-    //     let counter = 0;
-
-    //     spyOn(service, 'updateCounters').and.callThrough();
-    //     spyOn(service, 'updateStartingPointCounter');
-
-    //     service.updateCounters('acidgun', 'add');
-    //     expect(counter).toBe(1);
-
-    //     service.updateCounters('acidgun', 'remove');
-    //     expect(counter).toBe(0);
-    // });
-
-    it('should not update any counter if item is undefined', () => {
+    it('should add to startingPointCounter when action is "add" and item is "starting-point"', () => {
+        service.startingPointCounter = 2;
         spyOn(service, 'updateStartingPointCounter');
 
-        service.updateCounters(undefined, 'add');
+        service.updateCounters('starting-point', 'add');
+
+        expect(service.updateStartingPointCounter).toHaveBeenCalledWith(3);
+    });
+
+    it('should remove from startingPointCounter when action is "remove" and item is "starting-point"', () => {
+        service.startingPointCounter = 2;
+        spyOn(service, 'updateStartingPointCounter');
+
+        service.updateCounters('starting-point', 'remove');
+
+        expect(service.updateStartingPointCounter).toHaveBeenCalledWith(1);
+    });
+
+    it('should not call updateStartingPointCounter if the item is not "starting-point"', () => {
+        spyOn(service, 'updateStartingPointCounter');
+
+        service.updateCounters('some-other-item', 'add');
 
         expect(service.updateStartingPointCounter).not.toHaveBeenCalled();
+    });
+
+    it('should call next on startingPointCounterSource with the correct value', () => {
+        const spy = spyOn<any>(service['startingPointCounterSource'], 'next');
+
+        service.updateStartingPointCounter(5);
+
+        expect(spy).toHaveBeenCalledWith(5);
     });
 });
