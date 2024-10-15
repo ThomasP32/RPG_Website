f98234a6451d86e80c3797b85e48a952f89e3dd0
Thomas <thomas.perron-duveau@polymtl.ca>
2024-10-14 21:55:47 -0400

    fix(deployment): fix 2 url for server

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-3-96-63-155.ca-central-1.compute.amazonaws.com/api', // TODO: Add remote server URL
+    serverUrl: 'http://ec2-3-96-63-155.ca-central-1.compute.amazonaws.com:3000', // TODO: Add remote server URL
 };
