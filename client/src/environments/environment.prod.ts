db4f3ef94d8c79ba08f84a11f3bcfb98f43f0916
Thomas <thomas.perron-duveau@polymtl.ca>
2024-10-14 21:48:16 -0400

    fix(deployment): fix url for server

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://polytechnique-montr-al.gitlab.io/log2990/20243/equipe-109/LOG2990-109/api', // TODO: Add remote server URL
+    serverUrl: 'http://ec2-3-96-63-155.ca-central-1.compute.amazonaws.com/api', // TODO: Add remote server URL
 };
