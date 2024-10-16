580cc3fced3b3c8e53fa5b4307ed55e1a9a2301e
Thomas <thomas.perron-duveau@polymtl.ca>
2024-10-16 15:26:25 -0400

    fix(deployment): fix URL for images to show

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-3-96-63-155.ca-central-1.compute.amazonaws.com:3000/api', // TODO: Add remote server URL
+    serverUrl: 'http://ec2-3-96-63-155.ca-central-1.compute.amazonaws.com:3000/api',
 };
