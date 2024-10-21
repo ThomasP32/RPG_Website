e202c253f0534b351eccc3df252c80e364909f9f
txciturne <celineouchiha@gmail.com>
2024-10-21 17:35:36 -0400

    test(avatars) : changed numbers for enum Avatar

--- client/src/app/pages/character-form-page/character-form-page.component.spec.ts
+++ client/src/app/pages/character-form-page/character-form-page.component.spec.ts
@@ -7,49 +7,91 @@ import { Character } from '@app/interfaces/character';
 import { CharacterService } from '@app/services/character/character.service';
 import { SocketService } from '@app/services/communication-socket/communication-socket.service';
 import { CommunicationMapService } from '@app/services/communication/communication.map.service';
-import { Bonus } from '@common/game';
+import { Avatar, Bonus } from '@common/game';
 import { DBMap as Map, Mode } from '@common/map.types';
 import { Observable, of, Subject } from 'rxjs';
 import { CharacterFormPageComponent } from './character-form-page.component';
 
 const mockCharacters: Character[] = [
     {
-        id: 1,
+        id: Avatar.Avatar1,
         name: 'Alistair Clockhaven',
         image: '../../assets/characters/1.png',
         preview: '../../assets/previewcharacters/1.png',
         isAvailable: true,
     },
-    { id: 2, name: 'Arachnoform', image: '../../assets/characters/2.png', preview: '../../assets/previewcharacters/2.png', isAvailable: true },
-    { id: 3, name: 'Archibald Light', image: '../../assets/characters/3.png', preview: '../../assets/previewcharacters/3.png', isAvailable: true },
     {
-        id: 4,
+        id: Avatar.Avatar2,
+        name: 'Arachnoform',
+        image: '../../assets/characters/2.png',
+        preview: '../../assets/previewcharacters/2.png',
+        isAvailable: true,
+    },
+    {
+        id: Avatar.Avatar3,
+        name: 'Archibald Light',
+        image: '../../assets/characters/3.png',
+        preview: '../../assets/previewcharacters/3.png',
+        isAvailable: true,
+    },
+    {
+        id: Avatar.Avatar4,
         name: 'Archpriest Mechanohr',
         image: '../../assets/characters/4.png',
         preview: '../../assets/previewcharacters/4.png',
         isAvailable: true,
     },
-    { id: 5, name: 'Cyron Vex', image: '../../assets/characters/5.png', preview: '../../assets/previewcharacters/5.png', isAvailable: true },
-    { id: 6, name: 'Magnus Brassguard', image: '../../assets/characters/6.png', preview: '../../assets/previewcharacters/6.png', isAvailable: true },
     {
-        id: 7,
+        id: Avatar.Avatar5,
+        name: 'Cyron Vex',
+        image: '../../assets/characters/5.png',
+        preview: '../../assets/previewcharacters/5.png',
+        isAvailable: true,
+    },
+    {
+        id: Avatar.Avatar6,
+        name: 'Magnus Brassguard',
+        image: '../../assets/characters/6.png',
+        preview: '../../assets/previewcharacters/6.png',
+        isAvailable: true,
+    },
+    {
+        id: Avatar.Avatar7,
         name: 'Professor Quicksprocket',
         image: '../../assets/characters/7.png',
         preview: '../../assets/previewcharacters/7.png',
         isAvailable: true,
     },
     {
-        id: 8,
+        id: Avatar.Avatar8,
         name: 'Reginald Gearwhisle',
         image: '../../assets/characters/8.png',
         preview: '../../assets/previewcharacters/8.png',
         isAvailable: true,
     },
-    { id: 9, name: 'Vance Steelstrike', image: '../../assets/characters/9.png', preview: '../../assets/previewcharacters/9.png', isAvailable: true },
-    { id: 10, name: 'Zephyr Gearwind', image: '../../assets/characters/10.png', preview: '../../assets/previewcharacters/10.png', isAvailable: true },
-    { id: 11, name: 'Dr. Veselius', image: '../../assets/characters/11.png', preview: '../../assets/previewcharacters/11.png', isAvailable: true },
     {
-        id: 12,
+        id: Avatar.Avatar9,
+        name: 'Vance Steelstrike',
+        image: '../../assets/characters/9.png',
+        preview: '../../assets/previewcharacters/9.png',
+        isAvailable: true,
+    },
+    {
+        id: Avatar.Avatar10,
+        name: 'Zephyr Gearwind',
+        image: '../../assets/characters/10.png',
+        preview: '../../assets/previewcharacters/10.png',
+        isAvailable: true,
+    },
+    {
+        id: Avatar.Avatar11,
+        name: 'Dr. Veselius',
+        image: '../../assets/characters/11.png',
+        preview: '../../assets/previewcharacters/11.png',
+        isAvailable: true,
+    },
+    {
+        id: Avatar.Avatar12,
         name: 'Grimmauld Ironfist',
         image: '../../assets/characters/12.png',
         preview: '../../assets/previewcharacters/12.png',
