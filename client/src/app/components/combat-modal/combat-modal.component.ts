a54a5b6de54c3aa87d0c3125436b5314de666d96
Thomas <thomas.perron-duveau@polymtl.ca>
2024-10-21 22:25:44 -0400

    feat(combat): create combat component

---
+++ client/src/app/components/combat-modal/combat-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-combat-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './combat-modal.component.html',
+  styleUrl: './combat-modal.component.scss'
+})
+export class CombatModalComponent {
+
+}
