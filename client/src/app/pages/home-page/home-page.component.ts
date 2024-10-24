732675d2b3b96521497d4bd346214aed37b58e7b
Thomas <thomas.perron-duveau@polymtl.ca>
2024-10-23 22:07:26 -0400

    fix(combatGateway):fix connection with server

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { JoinGameModalComponent } from '@app/components/join-game-modal/join-game-modal.component';
 import { SocketService } from '@app/services/communication-socket/communication-socket.service';
-import { CombatModalComponent } from '../../components/combat-modal/combat-modal.component';
 /* eslint-disable no-unused-vars */
 @Component({
     selector: 'app-main-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss'],
-    imports: [RouterLink, JoinGameModalComponent, CombatModalComponent],
+    imports: [RouterLink, JoinGameModalComponent],
 })
 export class HomePageComponent implements OnInit {
     teamNumber = 'Équipe 109';
@@ -26,8 +25,8 @@ export class HomePageComponent implements OnInit {
         this.connect();
     }
 
-    // TODO:ca normalement ca ne devrait pas être necessaire sinon ca bloque la connexion seulement aux utilisateurs qui 
-    // ont accédé au jeu a partir de la page d'acceuil 
+    // TODO:ca normalement ca ne devrait pas être necessaire sinon ca bloque la connexion seulement aux utilisateurs qui
+    // ont accédé au jeu a partir de la page d'acceuil
     get socketId() {
         return this.socketService.socket && this.socketService.socket.id ? this.socketService.socket.id : '';
     }
