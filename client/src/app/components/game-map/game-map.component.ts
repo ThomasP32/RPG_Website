9e293da9ba1f9cc46fa48195a5583755316e2ad2
ldesmars <ldesmars2002@gmail.com>
2024-10-25 00:21:15 -0400

    feat(game-view) : linked to server

--- client/src/app/components/game-map/game-map.component.ts
+++ client/src/app/components/game-map/game-map.component.ts
@@ -1,6 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
+/* eslint-disable no-unused-vars */
+/* Les variables sont nécessaires pour afficher correctement le contenu dans le template. Elles sont utilisées dans des 
+fonctions comme getTileImage() et getItemImage() pour rendre la carte du jeu et les objets correctement.
+
+Le linter ne reconnaît pas l'utilisation dans le template et ne comprend pas que ces variables sont utilisées dans 
+le HTML et non directement dans le code TypeScript. En désactivant cette règle pour ces lignes, nous disons 
+à ESLint d'ignorer l'erreur, car nous savons que ces variables sont bien utilisées. */
+
 @Component({
     selector: 'app-game-map',
     standalone: true,
