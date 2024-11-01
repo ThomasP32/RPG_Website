8fc6f5a47a10b735bc74a40a620232ed53cc50e5
anismehenni <anxs.meh@gmail.com>
2024-11-01 02:30:34 -0400

    feature(waiting-room) : can see number of players in the room and max players

--- server/app/socket/game/gateways/game-creation/game-creation.gateway.ts
+++ server/app/socket/game/gateways/game-creation/game-creation.gateway.ts
@@ -54,25 +54,17 @@ export class GameGateway implements OnGatewayDisconnect {
         }
     }
 
-    @SubscribeMessage('getMapName')
-    getMapName(client: Socket, gameId: string): void {
-        if (this.gameCreationService.doesGameExist(gameId)) {
-            const game = this.gameCreationService.getGameById(gameId);
-            client.emit('currentMapName', game.name);
-        } else {
-            client.emit('gameNotFound', { reason: 'La partie a été fermée' });
-        }
-    }
     @SubscribeMessage('kickPlayer')
     handleKickPlayer(client: Socket, playerId: string): void {
         this.server.to(playerId).emit('playerKicked');
     }
 
-    @SubscribeMessage('getGame')
+    @SubscribeMessage('getGameData')
     getGame(client: Socket, gameId: string): void {
         if (this.gameCreationService.doesGameExist(gameId)) {
             const game = this.gameCreationService.getGameById(gameId);
-            client.emit('currentGame', game);
+            console.log('Game data:', game);
+            client.emit('currentGameData', { game: game, name: game.name, size: game.mapSize.x });
         } else {
             client.emit('gameNotFound', { reason: 'La partie a été fermée' });
         }
