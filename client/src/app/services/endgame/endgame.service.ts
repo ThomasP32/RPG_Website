269c9ea1557e651f0ac0d5c253058cf8644c9c7d
Thomas <thomas.perron-duveau@polymtl.ca>
2024-11-18 18:21:15 -0500

    fix(endgame): fix lint - fix constants

--- client/src/app/services/endgame/endgame.service.ts
+++ client/src/app/services/endgame/endgame.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MAX_CHAR, MINUTE, PERCENTAGE } from '@common/constants';
 import { Game, Player } from '@common/game';
 import { Coordinate } from '@common/map.types';
 
@@ -9,13 +10,13 @@ export class EndgameService {
     constructor() {}
 
     getPlayerTilePercentage(player: Player, game: Game): number {
-        return (player.visitedTiles.length / (game.mapSize.x * game.mapSize.y)) * 100;
+        return (player.visitedTiles.length / (game.mapSize.x * game.mapSize.y)) * PERCENTAGE;
     }
 
     gameDurationInMinutes(duration: number): string {
-        const minutes = Math.floor(duration / 60);
-        const seconds = duration % 60;
-        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        const minutes = Math.floor(duration / MINUTE);
+        const seconds = duration % MINUTE;
+        return `${minutes.toString().padStart(MAX_CHAR, '0')}:${seconds.toString().padStart(MAX_CHAR, '0')}`;
     }
 
     gameTilePercentage(game: Game): number {
