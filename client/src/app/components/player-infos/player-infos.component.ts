17074df5738a7e6d41aecc46b344092f5ab83dea
anismehenni <anxs.meh@gmail.com>
2024-11-29 02:09:15 -0500

    style(game) : inventory is right below players-info

--- client/src/app/components/player-infos/player-infos.component.ts
+++ client/src/app/components/player-infos/player-infos.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CharacterService } from '@app/services/character/character.service';
-import { Avatar, Bonus, Player } from '@common/game';
-import { ItemCategory } from '@common/map.types';
+import { ImageService } from '@app/services/image/image.service';
+import { Avatar, Player } from '@common/game';
 
 @Component({
     selector: 'app-player-infos',
@@ -11,35 +11,12 @@ import { ItemCategory } from '@common/map.types';
     styleUrl: './player-infos.component.scss',
 })
 export class PlayerInfosComponent {
-    player: Player = {
-        socketId: 'test-socket',
-        name: '123456789012345',
-        avatar: Avatar.Avatar1,
-        isActive: true,
-        position: { x: 0, y: 0 },
-        initialPosition: { x: 0, y: 0 },
-        specs: {
-            evasions: 2,
-            life: 3,
-            speed: 4,
-            attack: 2,
-            defense: 5,
-            movePoints: 5,
-            actions: 2,
-            attackBonus: Bonus.D4,
-            defenseBonus: Bonus.D6,
-            nVictories: 0,
-            nDefeats: 0,
-            nCombats: 0,
-            nEvasions: 0,
-            nLifeTaken: 0,
-            nLifeLost: 0,
-        },
-        inventory: [ItemCategory.TimeTwister, ItemCategory.Armor],
-        turn: 0,
-        visitedTiles: [],
-    };
-    constructor(private readonly characterService: CharacterService) {
+    @Input() player: Player;
+    constructor(
+        private readonly characterService: CharacterService,
+        protected readonly imageService: ImageService,
+    ) {
+        this.imageService = imageService;
         this.characterService = characterService;
     }
 
