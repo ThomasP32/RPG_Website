853c3e1309ecfc56bad4f14c6f7b152e5beac79b
Thomas <thomas.perron-duveau@polymtl.ca>
2024-11-29 15:54:13 -0500

    fix(QA): fix constants in endgame

--- client/src/app/services/endgame/endgame.service.ts
+++ client/src/app/services/endgame/endgame.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MAX_CHAR, MINUTE, PERCENTAGE } from '@common/constants';
+import { MAX_CHAR, MINUTE, PERCENTAGE, tableColumns } from '@common/constants';
 import { Game, GameCtf, Player } from '@common/game';
 import { Coordinate } from '@common/map.types';
 import { GameService } from '../game/game.service';
@@ -74,7 +74,7 @@ export class EndgameService {
         } else {
             this.gameService.game.players.sort((a, b) => b.specs.nCombats - a.specs.nCombats);
         }
-        this.sortColumn = 4;
+        this.sortColumn = tableColumns.COMBAT_COLUMN;
     }
 
     sortEvasions(): void {
@@ -84,7 +84,7 @@ export class EndgameService {
         } else {
             this.gameService.game.players.sort((a, b) => b.specs.nEvasions - a.specs.nEvasions);
         }
-        this.sortColumn = 5;
+        this.sortColumn = tableColumns.EVASION_COLUMN;
     }
 
     sortVictories(): void {
@@ -94,7 +94,7 @@ export class EndgameService {
         } else {
             this.gameService.game.players.sort((a, b) => b.specs.nVictories - a.specs.nVictories);
         }
-        this.sortColumn = 6;
+        this.sortColumn = tableColumns.VICTORIES_COLUMN;
     }
 
     sortDefeats(): void {
@@ -104,7 +104,7 @@ export class EndgameService {
         } else {
             this.gameService.game.players.sort((a, b) => b.specs.nDefeats - a.specs.nDefeats);
         }
-        this.sortColumn = 7;
+        this.sortColumn = tableColumns.DEFEATS_COLUMN;
     }
 
     sortLostLife(): void {
@@ -114,7 +114,7 @@ export class EndgameService {
         } else {
             this.gameService.game.players.sort((a, b) => b.specs.nLifeLost - a.specs.nLifeLost);
         }
-        this.sortColumn = 8;
+        this.sortColumn = tableColumns.LOST_LIFE_COLUMN;
     }
 
     sortStolenLife(): void {
@@ -124,7 +124,7 @@ export class EndgameService {
         } else {
             this.gameService.game.players.sort((a, b) => b.specs.nLifeTaken - a.specs.nLifeTaken);
         }
-        this.sortColumn = 9;
+        this.sortColumn = tableColumns.STOLEN_LIFE_COLUMN;
     }
 
     sortObjects(): void {
@@ -134,7 +134,7 @@ export class EndgameService {
         } else {
             this.gameService.game.players.sort((a, b) => b.specs.nItemsUsed - a.specs.nItemsUsed);
         }
-        this.sortColumn = 10;
+        this.sortColumn = tableColumns.OBJECT_COLUMN;
     }
 
     sortVisitedTiles(): void {
@@ -144,6 +144,6 @@ export class EndgameService {
         } else {
             this.gameService.game.players.sort((a, b) => b.visitedTiles.length - a.visitedTiles.length);
         }
-        this.sortColumn = 11;
+        this.sortColumn = tableColumns.TILES_COLUMN;
     }
 }
